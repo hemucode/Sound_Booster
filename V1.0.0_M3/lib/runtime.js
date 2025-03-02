3232b7424849fba5eab55abfc7133169e4ff98af
hemucode <69314427+hemucode@users.noreply.github.com>
2025-03-02 15:12:26 +0530

    ok

--- V1.0.0_M3/lib/runtime.js
+++ V1.0.0_M3/lib/runtime.js
@@ -1,22 +1,22 @@
 app.version = function () {return chrome.runtime.getManifest().version};
-app.homepage = function () {return chrome.runtime.getManifest().homepage_url};
+app.homepage = function () {return "https://www.codehemu.com/2025/03/SoundBooster.html"};
 
-// if (!navigator.webdriver) {
-//   app.on.uninstalled(app.homepage() +"#uninstall");
-//   app.on.installed(function (e) {
-//     app.on.management(function (result) {
-//       if (result.installType === "normal") {
-//         app.tab.query.index(function (index) {
-//           let previous = e.previousVersion !== undefined && e.previousVersion !== app.version();
-//           let doupdate = previous && parseInt((Date.now() - config.welcome.lastupdate) / (24 * 3600 * 1000)) > 45;
-//           if (e.reason === "install" || (e.reason === "update" && doupdate)) {
-//             let url = app.homepage();
-//             app.tab.open(url, index, e.reason === "install");
-//             config.welcome.lastupdate = Date.now();
-//           }
-//         });
-//       }
-//     });
-//   });
-// }
+if (!navigator.webdriver) {
+  app.on.uninstalled(app.homepage() +"#uninstall");
+  app.on.installed(function (e) {
+    app.on.management(function (result) {
+      if (result.installType === "normal") {
+        app.tab.query.index(function (index) {
+          let previous = e.previousVersion !== undefined && e.previousVersion !== app.version();
+          let doupdate = previous && parseInt((Date.now() - config.welcome.lastupdate) / (24 * 3600 * 1000)) > 45;
+          if (e.reason === "install" || (e.reason === "update" && doupdate)) {
+            let url = app.homepage();
+            app.tab.open(url, index, e.reason === "install");
+            config.welcome.lastupdate = Date.now();
+          }
+        });
+      }
+    });
+  });
+}
 
